78d27cde28429eaa2495408ef92107200754b263
Aqr <AquariusMy@outlook.com>
2017-05-24 10:16:30 +0800

    Aqr commit.

---
+++ app/src/main/java/com/example/aqr/homework/FiveInaRowView.java
@@ -0,0 +1,131 @@
+package com.example.aqr.homework;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Aqr on 2017/5/24.
+ */
+
+public class FiveInaRowView extends View {
+    private int mViewWidth;
+    private float mLineHeight;
+    private int MAX_LINE = 10;
+
+    private Bitmap mWhiteStone;
+    private Bitmap mBlackStone;
+
+    private float ratio = 4*1.0f/5;//3*1.0f/4
+    private List<Point> downWhitePoint = new ArrayList<>();
+    private List<Point> downBlackPoint = new ArrayList<>();
+
+    private boolean isWhite = false;
+
+    private Paint mPaint = new Paint();
+    public FiveInaRowView(Context context) {
+        super(context);
+        setBackgroundColor(0x44ff0000);
+        init();
+    }
+
+    public FiveInaRowView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setBackgroundColor(0x44ff0000);
+        init();
+    }
+
+    public FiveInaRowView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setBackgroundColor(0x44ff0000);
+        init();
+    }
+    public void init(){
+        mPaint.setColor(0x88000000);
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mWhiteStone = BitmapFactory.decodeResource(getResources(), R.drawable.ic_stone_white);
+        mBlackStone= BitmapFactory.decodeResource(getResources(), R.drawable.ic_stone_black);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int width = Math.min(widthSize,heightSize);
+
+        if(widthMode == MeasureSpec.UNSPECIFIED){
+            width = heightSize;
+        }
+        else if(heightMode == MeasureSpec.UNSPECIFIED){
+            width = widthSize;
+        }
+        setMeasuredDimension(width,width);
+
+       // super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mViewWidth = w;
+        mLineHeight = mViewWidth*1.0f/MAX_LINE;
+
+        int stoneWidth = (int) (mLineHeight*ratio);
+
+        mWhiteStone = Bitmap.createScaledBitmap(mWhiteStone, stoneWidth, stoneWidth,false);
+        mBlackStone = Bitmap.createScaledBitmap(mBlackStone, stoneWidth, stoneWidth,false);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        if(action ==  MotionEvent.ACTION_DOWN){
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            Point p = new Point(x, y);
+            if(isWhite){
+                downWhitePoint.add(p);
+            }
+            else{
+                downBlackPoint.add(p);
+            }
+            invalidate();
+            isWhite = !isWhite;
+            return true;
+        }
+        return super.onTouchEvent(event);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawBroad(canvas);
+    }
+    private void drawBroad(Canvas canvas){
+        int w = mViewWidth;
+        float lineHeight = mLineHeight;
+        for(int i = 0; i<MAX_LINE; i++){
+            int startX = (int)(lineHeight/2);
+            int endX = (int) (w -lineHeight/2);
+            int y = (int) ((0.5+i)*lineHeight);
+            canvas.drawLine(startX, y, endX, y,mPaint);
+            canvas.drawLine(y, startX, y, endX, mPaint);
+        }
+    }
+
+}
